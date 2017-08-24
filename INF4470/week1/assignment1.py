a7266a313f27f5b8f0140463f4ee96e5a3cf0d48
Mathias <mathias.kirkerod@gmail.com>
2017-08-24 14:18:15 +0200

    changes

--- INF4470/week1/assignment1.py
+++ INF4470/week1/assignment1.py
@@ -1,2 +1,26 @@
 import jupyter 
+from numpy import *
+import matplotlib.pyplot as plt
+
+t=linspace(-1,2.5,1000)
+
+plt.subplot(2,2,1)
+func=cos(2*pi*t)
+plt.plot(t,func)
+
+plt.subplot(2,2,2)
+func=cos(2*pi*t+pi)
+plt.plot(t,func)
+
+plt.subplot(2,2,3)
+func=cos(8*pi*t)
+plt.plot(t,func)
+
+plt.subplot(2,2,4)
+func=cos(4*pi*t- pi/3)
+plt.plot(t,func)
+
+
+plt.show()
+
 print('hello world')

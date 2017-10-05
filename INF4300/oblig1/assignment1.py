046735fa80cf9dfba502b2c36eaa29c0bac3e95c
Mathias <mathias.kirkerod@gmail.com>
2017-10-05 10:34:58 +0200

    new day

--- INF4300/oblig1/assignment1.py
+++ INF4300/oblig1/assignment1.py
@@ -12,10 +12,6 @@ from funcs import *
 plot1=False
 plot2=False
 
-
-
- 
-
 img0 = cv2.imread('mosaic1.png',0)
 img1 = cv2.imread('mosaic2.png',0)
 #img1 = cv2.imread('zebra_3.tif',0)
@@ -23,12 +19,6 @@ img0shape=img0.shape
 img1shape=img1.shape
 
 
-if plot1:
-    plt.subplot(121),plt.imshow(img0,cmap = 'gray')
-    plt.title('Original Image1'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(img1,cmap = 'gray')
-    plt.title('Original Image2'), plt.xticks([]), plt.yticks([])
-    plt.show()
 
 #splitting the images
 #assuming that the image is L/2 in with and height
@@ -40,6 +30,8 @@ subimg10=img1[0:256 , 0:256 ]
 subimg11=img1[0:256 , 256:512 ]
 subimg12=img1[256:512 , 0:256 ]
 subimg13=img1[256:512 , 256:512 ]
+original_img=[subimg00,subimg01,subimg02,subimg03,subimg10,subimg11,subimg12,subimg13,img0,img1]
+#original_img is now on the sape over
 
 
 #from this point, every transformation will be in lists:
@@ -55,52 +47,50 @@ element in list is described under
 
 """
 
-original_img=[subimg00,subimg01,subimg02,subimg03,subimg10,subimg11,subimg12,subimg13,img0,img1]
-#original_img is now on the sape over
-for i in enumerate(original_img):
-    break
-    plt.imshow(i[1],cmap='gray')
-    a="%s"%(i[0])
-    plt.title(a,)
-    plt.savefig('report/%s.png'%(i[0]))
-    plt.clf()
-
-    plt.imshow(ski.feature.canny(i[1]),cmap='gray')
-    a="canny%s"%(i[0])
-    plt.title(a,)
-    plt.savefig('report/canny%s.png'%(i[0]))
-    plt.clf()
-
-    plt.hist(i[1].ravel(), bins=256, histtype='step', color='black') 
-    a="hist%s"%(i[0])
-    plt.title(a,)
-    plt.savefig('report/hist%s.png'%(i[0]))
-    plt.clf()
-  
-   
 
 
-"""
-EQ IMG
-"""
-"""
-equalized_img=[]
-for i in original_img:
-        equalized_img.append(ski.exposure.rescale_intensity(i, out_range=(0, 15)))
-        #equalized_img.append(cv2.equalizeHist(i))
 
-plot_imglist(equalized_img,sub=True)
-"""
 
+
+###################
+######PART 1#######
+###################
+
+def oppg1():
+    if plot1:
+        plt.subplot(121),plt.imshow(img0,cmap = 'gray')
+        plt.title('Original Image1'), plt.xticks([]), plt.yticks([])
+        plt.subplot(122),plt.imshow(img1,cmap = 'gray')
+        plt.title('Original Image2'), plt.xticks([]), plt.yticks([])
+        plt.show()
+    for i in enumerate(original_img):
+        plt.imshow(i[1],cmap='gray')
+        a="%s"%(i[0])
+        plt.title(a,)
+        plt.savefig('report/%s.png'%(i[0]))
+        plt.clf()
+    
+        plt.imshow(ski.feature.canny(i[1]),cmap='gray')
+        a="canny%s"%(i[0])
+        plt.title(a,)
+        plt.savefig('report/canny%s.png'%(i[0]))
+        plt.clf()
+    
+        plt.hist(i[1].ravel(), bins=256, histtype='step', color='black') 
+        a="hist%s"%(i[0])
+        plt.title(a,)
+        plt.savefig('report/hist%s.png'%(i[0]))
+        plt.clf()
+      
+       
+    
+    
 
 
 ###################
 ######PART 2#######
 ###################
 
-"""
-GLCM IMG
-"""
 def oppg2():
     glcm_img=[]
     
@@ -264,96 +254,37 @@ def oppg2():
 ######PART 3#######
 ###################
 
-#@jit
-def homogenity(p):
-    rows,cols=p.shape
-    a=np.fromfunction(lambda x, y: 1/(1+(x-y)**2) , (rows, cols), dtype=int)
-    return np.sum(np.multiply(a,p))
+def oppg3():
+    inertia_attr=[5,5]#[5,5,5,5,5,5,5,5,5,5,5]
+    homogenity_attr=[5,5]#[5,5,5,5,5,5,5,5,5]
+    cluster_shade_attr=[5,5]#[5,5,5,5,5,5,5,5,5,5]
+    inertia_img,homogenity_img,cluster_shade_img=save_features(original_img[-1], inertia_attr, homogenity_attr, 
+                 cluster_shade_attr)
+
+    return inertia_img,homogenity_img,cluster_shade_img
+###################
+######PART 4#######
+###################
+
+def oppg4():
+    ski.filters.try_all_threshold(inertia_img[9],figsize=(19,19),verbose=False)
+    plt.savefig("report/try_all_thr9")
+    plt.show()
+    ski.filters.try_all_threshold(inertia_img[8],figsize=(19,19),verbose=False)
+    plt.savefig("report/try_all_thr8")
+    plt.show()
+
+    #applying global threshold
     
-def energy(p):
-    p=np.power(p, 2)
-    return np.sum(p)
-
-def inertia(p):
-    rows,cols=p.shape
-    a=np.fromfunction(lambda x, y:(x-y)**2 , (rows, cols), dtype=int)
-    return np.sum(np.multiply(a,p))
-   
-def cluster_shade(p):
-    rows,cols=p.shape
+    #fist image has a good result with almost all the global theshold algortihms: using OTSU
+    tresh=ski.filters.threshold_otsu(inertia_img)
+    img=inertia_img<=tresh 
     
-    def u_x(rows,cols,p):
-        
-        return np.fromfunction(lambda  i,j : i*np.sum(p[i,:]) , (rows,1), dtype=int)
+    plt.imshow(img)
+    plt.title("Standard  Otsu on inertia_img_0")
+    plt.show()
     
-    def u_y(rows,cols,p):
-        return np.fromfunction(lambda  j,i : j*np.sum(p[:,j]) , (cols,1), dtype=int)
     
-    a=np.fromfunction(lambda x, y:x+y , (rows, cols), dtype=int)
-    b=u_x(rows,cols,p)
-    c=u_y(rows,cols,p)
-    d=np.add(a, b)
-    d=np.add(d,c)
-    d=np.power(d, 3)
-    return np.sum(np.multiply(d,p))
     
-
-def get_coords(size,length,x,inarray):
-    s=int((size-1)/2)
-    arr=[]
-    for i in range(s,length-size):
-        tmp=inarray[i-s:i+s,x-s:x+s]
-        arr.append(tmp.tolist())
-    return arr
-"""    
-def sliding_window_v(inarray,size,f):
-    equalized0=cv2.equalizeHist(inarray)
-    inarray_16=(ski.exposure.rescale_intensity(equalized0, out_range=(0, 15)))
-    s=int((size-1)/2)
-    A,B=np.shape(inarray)
-    newarray=np.zeros(inarray.shape)
-    
-    for i in range(A):
-        if i+s >= A or i-s <= 0:
-            continue
-        pntlist=get_coords(size, B, i, inarray_16)
-        for j in enumerate(pntlist):
-            tmp=GLCM(j[1],[3],[0],symmetric1=True, normed1=True) 
-            newarray[i][j[0]]=f(tmp[:,:,0,0])
-    return newarray[0+s+1:A-s,0+s+1:B-s]    
-"""    
-
-def sliding_window(inarray,size,f):
-    equalized0=cv2.equalizeHist(inarray)
-    inarray_16=(ski.exposure.rescale_intensity(equalized0, out_range=(0, 15)))
-    s=int((size-1)/2)
-    newarray=np.zeros(inarray.shape)
-    A,B=np.shape(inarray)
-    
-    for i in enumerate(inarray):
-        if i[0]+s >= A or i[0]-s <= 0:
-            continue
-        for j in enumerate(i[1]):
-            if j[0]+s >= B or j[0]-s <= 0:
-                continue
-            
-            
-            w=[i[0]-s,i[0]+s,j[0]-s,j[0]+s]
-            p=GLCM(inarray_16[w[0]:w[1],w[2]:w[3]], [3], [0], symmetric1=True, normed1=True)
-            p=p[:,:,0,0] #gets only the first gclm
-            newarray[i[0]][j[0]]=f(p)
-    
-    return newarray[0+s+1:A-s,0+s+1:B-s]    
-
-
-import time
-
-start_time = time.time()
-b=sliding_window(original_img[-1],5,energy)
-print("--- %s seconds ---" % (time.time() - start_time))
-
-plt.imshow(b)
-plt.show()
-
-
-#make_gif(equalized_img[2],20,10)
+inertia_img,homogenity_img,cluster_shade_img=oppg3()   
+oppg4()
--- INF4300/oblig1/funcs.py
+++ INF4300/oblig1/funcs.py
@@ -8,8 +8,6 @@ from matplotlib import animation
 import sys
 from numpy import pi
 from numba import jit
-from funcs import *
-#import seaborn
 
 def make_gif(image,num_points,length):
     points=np.linspace(0,np.pi,num_points)
@@ -68,4 +66,97 @@ def plot_imglist(img,sub=False):
         plt.title('Img2'), plt.xticks([]), plt.yticks([])
         plt.show()
 
-       
+
+def save_features(original_img,inertia_attr,homogenity_attr,cluster_shade_attr): 
+    inertia_img=[]
+    homogenity_img=[]
+    cluster_shade_img=[]
+    import time
+    for i in enumerate(original_img):
+        start_time = time.time()
+        a=sliding_window(i[1],inertia_attr[i[0]],inertia)
+        b=sliding_window(i[1],homogenity_attr[i[0]],homogenity)
+        c=sliding_window(i[1],cluster_shade_attr[i[0]],cluster_shade)
+        inertia_img.append(a)
+        homogenity_img.append(b)
+        cluster_shade_img.append(c)
+        
+        print("--- %s seconds ---" % (time.time() - start_time))
+    
+        plt.imshow(a)
+        plt.savefig('report/inertia%s.png'%(i[0]))
+        plt.clf()
+        plt.imshow(b)
+        plt.savefig('report/homogenity%s.png'%(i[0]))
+        plt.clf()
+        plt.imshow(c)
+        plt.savefig('report/cluster_shade%s.png'%(i[0]))
+        plt.clf()
+    
+    return inertia_img,homogenity_img,cluster_shade_img       
+
+
+
+def eq_img(original_img,plotting=False):    
+    #equalized img with 16 greysacles
+    equalized_img=[]
+    for i in original_img:
+            equalized_img.append(ski.exposure.rescale_intensity(cv2.equalizeHist(i), out_range=(0, 15)))
+    
+    if plotting:
+        plot_imglist(equalized_img,sub=True)
+    return equalized_img
+
+
+def homogenity(p):
+        rows,cols=p.shape
+        a=np.fromfunction(lambda x, y: 1/(1+(x-y)**2) , (rows, cols), dtype=int)
+        return np.sum(np.multiply(a,p))
+        
+    
+def inertia(p):
+    rows,cols=p.shape
+    a=np.fromfunction(lambda x, y:(x-y)**2 , (rows, cols), dtype=int)
+    return np.sum(np.multiply(a,p))
+   
+def cluster_shade(p):
+    rows,cols=p.shape
+    
+    def u_x(rows,cols,p):
+        
+        return np.fromfunction(lambda  i,j : i*np.sum(p[i,:]) , (rows,1), dtype=int)
+    
+    def u_y(rows,cols,p):
+        return np.fromfunction(lambda  j,i : j*np.sum(p[:,j]) , (cols,1), dtype=int)
+    
+    a=np.fromfunction(lambda x, y:x+y , (rows, cols), dtype=int)
+    b=u_x(rows,cols,p)
+    c=u_y(rows,cols,p)
+    d=np.add(a, b)
+    d=np.add(d,c)
+    d=np.power(d, 3)
+    return np.sum(np.multiply(d,p))
+    
+@jit
+def sliding_window(inarray,size,f):
+    equalized0=cv2.equalizeHist(inarray)
+    inarray_16=(ski.exposure.rescale_intensity(equalized0, out_range=(0, 15)))
+    s=int((size-1)/2)
+    newarray=np.zeros(inarray.shape)
+    A,B=np.shape(inarray)
+    
+    
+    for i in enumerate(inarray):
+        if i[0]+s >= A or i[0]-s <= 0:
+            continue
+        for j in enumerate(i[1]):
+            if j[0]+s >= B or j[0]-s <= 0:
+                continue
+            
+            
+            w=[i[0]-s,i[0]+s,j[0]-s,j[0]+s]
+            p=GLCM(inarray_16[w[0]:w[1],w[2]:w[3]], [3], [0], symmetric1=True, normed1=True)
+            p=p[:,:,0,0] #gets only the first gclm
+            newarray[i[0]][j[0]]=f(p)
+    
+    return newarray[0+s+1:A-s,0+s+1:B-s]    

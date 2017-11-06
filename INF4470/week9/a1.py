a654c9e2886664cba2fbe1e9e21686196adc1ac4
Mathias <mathias.kirkerod@gmail.com>
2017-11-06 17:05:19 +0100

    ja

--- INF4470/week9/a1.py
+++ INF4470/week9/a1.py
@@ -23,9 +23,35 @@ for i in [0,0.25,0.5,0.75,1]:
     tuk=w(2.8, i)
     v=np.vectorize(tuk) 
     pl.append((v(t),i))
-
+"""
 for l in pl:
     plt.plot(t,l[0])
 
 plt.legend(["r={}".format(l[1]) for l in pl]) 
 plt.show()
+"""
+##############
+#### 7.57 ####
+##############
+
+
+        
+def win(t):
+    ret=0
+    for k in range(1,K+1):
+        Fk=f_l+(k-1)*(f_h-f_l)/(K-1)
+        ret+=np.cos(2*np.pi*Fk*t)
+    return ret
+        
+
+f_l=40
+f_h=60
+K=10
+t=np.linspace(-100,100)
+a=abs(np.fft.fft(win(t)))
+
+plt.plot(t,a)
+plt.show()
+
+:
+    
